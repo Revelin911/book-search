93267151f4aae697557f3cd0ca15bc7b756c4b32
Revelin911 <revelin911@mail.com>
2025-03-25 17:54:40 -0600

    updating middleware to use graphql

--- server/src/services/auth.ts
+++ server/src/services/auth.ts
@@ -1,39 +1,38 @@
-import type { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { GraphQLError } from 'graphql';
 import dotenv from 'dotenv';
 dotenv.config();
 
-interface JwtPayload {
-  _id: unknown;
-  username: string;
-  email: string,
-}
+//Update the auth middleware function to work with the GraphQL API
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization;
+export const authenticateToken = ({req}: any) => {
+  let authHeader = req.body.token || req.query.token || req.headers.authorization;
 
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
-
-    const secretKey = process.env.JWT_SECRET_KEY || '';
-
-    jwt.verify(token, secretKey, (err, user) => {
-      if (err) {
-        return res.sendStatus(403); // Forbidden
-      }
-
-      req.user = user as JwtPayload;
-      return next();
-    });
-  } else {
-    res.sendStatus(401); // Unauthorized
+  if (req.headers.authorization) {
+    authHeader = authHeader.split(' ').pop().trim();
   }
-};
+  if (!authHeader) {
+    return req;
+  }
+try {
+    const { data }: any = jwt.verify(authHeader, process.env.JWT_SECRET_KEY || '', { maxAge: '1hr'});
+req.user = data;
+} catch (err) {
+      console.log('Invalid token');
+      }
+      return req;
+    };
 
 export const signToken = (username: string, email: string, _id: unknown) => {
   const payload = { username, email, _id };
-  const secretKey = process.env.JWT_SECRET_KEY || '';
+  const secretKey: any = process.env.JWT_SECRET_KEY;
 
-  return jwt.sign(payload, secretKey, { expiresIn: '1h' });
+  return jwt.sign({ data: payload }, secretKey, { expiresIn: '1h' });
+};
+
+export class AuthenticationError extends GraphQLError {
+  constructor(message: string) {
+    super(message, undefined, undefined, undefined, ['UNAUTHENTICATED']);
+    Object.defineProperty(this, 'name', {value: 'AuthenticaionError'});
+  }
 };
