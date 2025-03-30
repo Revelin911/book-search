070426ecd02b598c12795b43b530a2753c98f4d7
Revelin911 <revelin911@mail.com>
2025-03-29 19:53:05 -0600

    added apollo server to work in app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,13 +8,33 @@ import {
 } from 'apollo/client';
 
 import Navbar from './components/Navbar';
+import { setContext } from '@apollo/client/link/context';
+
+// Connect HTTP link to GraphQL server
+const httpLink = createHttpLink({ uri: '/graphql',});
+
+// Attach JWT token to requests
+const authLink = setContext((_, { headers }) => {
+const token = localStorage.getItem('id_token');
+return { 
+  headers: {
+    ...headers,
+    authorization: token ? `Bearer ${token}` : '',
+  },
+};
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
